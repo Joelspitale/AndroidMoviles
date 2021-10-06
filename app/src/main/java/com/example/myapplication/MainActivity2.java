915650a8aea5faf7804e3cd877fff271cfe22b6d
Matiarriete <47977071+Matiarriete@users.noreply.github.com>
2021-10-06 13:03:01 -0300

    Camara Andando

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,10 +1,15 @@
 package com.example.myapplication;
 
+import static android.Manifest.permission.CAMERA;
 import static com.example.myapplication.SingIn.MY_INTENT_ACTIVITY_VALUE;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +17,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    static final int REQUEST = 1;   // lo seteo siempre que uso el activityForResult
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,45 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
+        Button botonCamara =  findViewById(R.id.buttonCamara);
+        if(verifyCamara())
+        botonCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // el intent que creo aca le seteo su resultado y lo devuelvo a la activity que lo llamo
+                Intent myIntent = new Intent(MainActivity2.this, CamaraPrueba.class);
+                startActivityForResult(myIntent, REQUEST);
+                finish();
+
+            }
+        });
+
+
+    }
+
+    private boolean verifyCamara() {
+        if(checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(CAMERA)) {
+            cargarDialogoRecomendacion();
+        } else {
+            requestPermissions(new String[]{CAMERA}, 100);
+        }
+        return false;
+    }
+
+    private void cargarDialogoRecomendacion() {
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity2.this);
+        dialogo.setTitle("Permisos desactivados");
+        dialogo.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la app");
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                requestPermissions(new String[]{CAMERA}, 100);
+            }
+        });
+        dialogo.show();
     }
 
     //en caso de que el usuario me la mande al fondo a la app se rompe mi app, para ello uso este metodo
