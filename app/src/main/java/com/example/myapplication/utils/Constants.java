5b6cb626b84d827da70b4019c8ba5a5531eaf3cf
JoelSpitale97 <42243492+Joelspitale@users.noreply.github.com>
2021-11-24 20:45:59 -0300

    Update Constants.java

--- app/src/main/java/com/example/myapplication/utils/Constants.java
+++ app/src/main/java/com/example/myapplication/utils/Constants.java
@@ -1,8 +1,8 @@
 package com.example.myapplication.utils;
 
 public class Constants {
-    public  static final String EMAIL = "majoarrispi9@gmail.com";
-    public static final String PASSWORD = "@rrispim4jo2017";
+    public  static final String EMAIL = "";
+    public static final String PASSWORD = "";
     public static final  int MIN_SIZE_PASSWORD = 8;
     public static final  int MAX_SIZE_PASSWORD = 20;
 }
